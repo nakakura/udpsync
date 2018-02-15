3870dadd10ae38f185249bd1800aeb17f400fa27
Toshiya Nakakura <nakakura@me.com>
2018-02-15 13:49:14 +0900

    update testscript

--- testscripts/simple/redirect.rb
+++ testscripts/simple/redirect.rb
@@ -1,14 +1,12 @@
 require 'socket'
 
-target = "127.0.0.1"
-port = 10000
 sender    = UDPSocket.new
 
 s    = UDPSocket.new
-port = 9000
+port = ARGV[2]
 s.bind("0.0.0.0", port)
 
 while true
   ar = s.recvfrom(65535)
-  sender.send(ar[0], 0, target, 10000)
+  sender.send(ar[0], 0, ARGV[0], ARGV[1])
 end
