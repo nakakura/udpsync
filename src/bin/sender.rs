2dc55c7436bf0bcbd1dfd9dde5de9260c4a7d42e
Toshiya Nakakura <nakakura@me.com>
2018-02-20 11:26:19 +0900

    bug fix

--- src/bin/sender.rs
+++ src/bin/sender.rs
@@ -12,16 +12,17 @@ use chrono::*;
 use std::net::SocketAddr;
 
 fn main() {
+    let target_addr = "192.168.20.101";
     //recv and redirect rtp from gstreamer
     let bind_addr_rtp: SocketAddr = format!("0.0.0.0:{}", 10000).parse().unwrap();
-    let target_addr_rtp: SocketAddr = format!("192.168.20.101:{}", 20000).parse().unwrap();
+    let target_addr_rtp: SocketAddr = format!("{}:{}", target_addr, 20000).parse().unwrap();
     let (recv_rtp_tx, recv_rtp_rx) = mpsc::channel::<Vec<u8>>(5000);
     let th_rtp_1 = udpsync::udp::receiver(bind_addr_rtp, recv_rtp_tx);
     let th_rtp_2 = udpsync::udp::sender(recv_rtp_rx.map(move |x| (target_addr_rtp, x)));
 
     //recv and redirect data from hapt sensor
     let bind_addr_hapt: SocketAddr = format!("0.0.0.0:{}", 10001).parse().unwrap();
-    let target_addr_hapt: SocketAddr = format!("192.168.20.101:{}", 20001).parse().unwrap();
+    let target_addr_hapt: SocketAddr = format!("{}:{}", target_addr, 20001).parse().unwrap();
     let (recv_hapt_tx, recv_hapt_rx) = mpsc::channel::<Vec<u8>>(5000);
     let th_hapt_1 = udpsync::udp::receiver(bind_addr_hapt, recv_hapt_tx);
     let th_hapt_2 = udpsync::udp::sender(recv_hapt_rx.map(move |x| {
